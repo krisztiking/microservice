d42012705b54069f3af24ca93e23ff186944d9c6
Krisztian Domokos <domokoskrisztian7@gmail.com>
2024-10-23 12:36:23 +0200

    products and JSON formatting created

--- main.go
+++ main.go
@@ -1,19 +1,56 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	handlers "github.com/krisztiking/microservices/handlers"
 )
 
+//var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
 func main() {
+
+	//env.Parse()
+
 	l := log.New(os.Stdout, "prodict-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
+	//hh := handlers.NewHello(l)
+	//gh := handlers.NewGoodbye(l)
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
+	sm.Handle("/", ph)
+	//sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		//Addr:         *bindAddress,
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Recieved terminate, graceful shoutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+
+	//	http.ListenAndServe(":9090", sm)
 }
